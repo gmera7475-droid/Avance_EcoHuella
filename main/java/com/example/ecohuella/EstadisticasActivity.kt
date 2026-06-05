9de5f90bc86279feb77ed8f319a68ef4a6ee3f79
gmera7475-droid <gmera7475@gmail.com>
2026-06-04 19:30:25 -0500

    Update EstadisticasActivity.kt

--- main/java/com/example/ecohuella/EstadisticasActivity.kt
+++ main/java/com/example/ecohuella/EstadisticasActivity.kt
@@ -6,6 +6,5 @@ import androidx.appcompat.app.AppCompatActivity
 class EstadisticasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Layout will be added later or just show a blank screen for now
     }
 }
