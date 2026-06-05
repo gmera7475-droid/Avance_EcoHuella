a1939e76ff3bb80b20b64e098cadb4c3135d9461
gmera7475-droid <gmera7475@gmail.com>
2026-06-04 19:30:05 -0500

    Update LogrosActivity.kt

--- main/java/com/example/ecohuella/LogrosActivity.kt
+++ main/java/com/example/ecohuella/LogrosActivity.kt
@@ -6,6 +6,5 @@ import androidx.appcompat.app.AppCompatActivity
 class LogrosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Layout will be added later or just show a blank screen for now
     }
 }
